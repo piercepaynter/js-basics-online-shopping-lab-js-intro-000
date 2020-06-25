b4043b7c6cd8e2c5b027a267c0cd78816dfc546d
Pierce Paynter <paynterpierce@gmail.com>
2020-06-25 02:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,9 +69,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === "") {
-    return "Sorry, we don't have a credit card on file for you."
+    return "Sorry, we don't have a credit card on file for you.";
   }
   else {
-    return `Yuor total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    cart = [];
+    return `Yuor total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
 }
