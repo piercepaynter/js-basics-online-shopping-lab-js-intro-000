604a4600a8d64f3984a855015f4addee0bb162b6
Pierce Paynter <paynterpierce@gmail.com>
2020-06-25 03:06:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -74,5 +74,6 @@ function placeOrder(cardNumber) {
   else {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     cart = [];
+    return cart;
   }
 }
