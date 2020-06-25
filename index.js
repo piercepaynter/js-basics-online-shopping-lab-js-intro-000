93ea8d3747223ef8e3090ca13cf67003459f9375
Pierce Paynter <paynterpierce@gmail.com>
2020-06-25 03:09:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -72,8 +72,9 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else {
+    var totalCost = total();
     cart.splice(0)
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
     //cart.splice(0)
     //return cart;
   }
