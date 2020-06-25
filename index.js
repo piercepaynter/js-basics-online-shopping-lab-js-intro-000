c040e54f07a5e52775bcda04ae81cb8cb281672f
Pierce Paynter <paynterpierce@gmail.com>
2020-06-25 02:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,22 +54,17 @@ function total() {
 }
 
 function removeFromCart(item) {
-  //if (item != cart.itemName) {
-    //return "That item is not in your cart."
-  //}
-  //else {
-    var x = 0;
-    for (let i=0; i < cart.length; i++) {
-      if (getCart()[i].itemName = item) {
-        getCart().splice(i, 1);
-        x = 1;
-        return cart;
-      }
-    if (x === 0) {
-      return "That item is not in your cart."
+  var x = 0;
+  for (let i=0; i < cart.length; i++) {
+    if (getCart()[i].itemName = item) {
+      getCart().splice(i, 1);
+      x = 1;
+      return cart;
     }
-    }
-  //}
+  }
+  if (x === 0) {
+    return "That item is not in your cart."
+  }
 }
 
 function placeOrder(cardNumber) {
