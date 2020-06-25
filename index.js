b33e2e651fab09781815da14643114f717e9a23a
Pierce Paynter <paynterpierce@gmail.com>
2020-06-25 02:52:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
   for (let i=0; i < cart.length; i++) {
     if (getCart()[i].itemName = item) {
       cart.splice(i, 1);
-      x = 1;
+      x === 1;
       return getCart();
     }
   }
