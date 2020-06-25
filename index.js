30f10b2c96cb3ff435ea728fb9b4dcdd53479b91
Pierce Paynter <paynterpierce@gmail.com>
2020-06-25 02:57:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-
+    return `Yuor total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
